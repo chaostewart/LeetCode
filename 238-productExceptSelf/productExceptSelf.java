cfddc62861718a3197908323f569a2b70c96f339
Chelsea (Chao) Li <chelseachaoli@gmail.com>
2018-06-07 14:40:40 -0700

    Add files via upload

---
+++ 238-productExceptSelf/productExceptSelf.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] output = new int[len];   
+        output[0] = 1;   // initilize 1st output elem to be 1
+        // the following loop calculates the product of all elems on the left side of each elem i
+        for (int i = 1; i < len; i++) {
+            output[i] = output[i-1] * nums[i-1];
+        }
+        int right = 1;
+        // the following loop then includes the product of all elems on the right side of each elem i
+        for (int j = len - 1; j >= 0; j--) {
+            output[j] *= right;
+            right *= nums[j];
+        }
+        return output;
+    }
+}
