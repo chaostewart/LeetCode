4d7cd768047e48023bbd7dcef71f6f231d76820d
Chelsea (Chao) Li <chelseachaoli@gmail.com>
2018-06-10 12:45:33 -0700

    Add files via upload

---
+++ 235-lowestCommonAncestor/lowestCommonAncestor.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // this is not just a binary tree...a BST has its left child smaller than it, and right child larger than it
+        // here comes a genius's code...OMG..
+        while((root.val - p.val) * (root.val - q.val) > 0) {
+            root = root.val > p.val ? root.left : root.right;
+        }
+        return root;
+    }
+}
