bff30b2b47df7cb592ec220121f082b3176de22e
Chelsea (Chao) Li <chelseachaoli@gmail.com>
2018-06-10 13:26:44 -0700

    Add files via upload

---
+++ 167-TwoSumWithSortedArray/twoSum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        // two pointers
+        int[] res = new int[2];
+        if (numbers == null || numbers.length < 2) return res;
+        int left = 0, right = numbers.length - 1;
+        while (left < right) {   // keep moving two pointers towards each other
+            if (numbers[left] + numbers[right] == target) {
+                res[0] = ++left; res[1] = ++right;   // answers are not zero-based
+                return res;
+            }
+            else if (numbers[left] + numbers[right] > target)
+                right--;
+            else 
+                left++;
+        }
+        return res;
+    }
+}
