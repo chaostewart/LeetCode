8c3d3f80f7a861d526b33a38215f5da5b1d3d951
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-06-11 23:02:14 -0700

    Add files via upload

---
+++ 240-Search2DMatrixII/searchMatrix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length < 1 || matrix[0].length < 1) return false;
+        // This genius algo. starts the search from top right!!  Time complexity is O(m + n)
+        int row = 0, col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == target) 
+                return true;
+            else if (target < matrix[row][col])  // move towards left if target < current number
+                col--;
+            else       // move downwards if target > current number
+                row++;
+        }
+        return false;
+    }
+}
