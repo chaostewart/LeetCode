6aab831c61c4e16c19b57766eb7728d1585304af
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-06-18 19:11:50 -0700

    Add files via upload

---
+++ 199. BinaryTreeRightSideView/rightSideView.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {        
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) return ans;
+        // BFS starts here
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            for (int i = 0; i < levelSize; i++) {
+                if (i == levelSize - 1) ans.add(queue.peek().val);   // get the value of the rightmost node at each level
+                if (queue.peek().left != null) queue.offer(queue.peek().left);
+                if (queue.peek().right != null) queue.offer(queue.peek().right);
+                queue.poll();
+            }
+        }
+        return ans;
+    }
+}
