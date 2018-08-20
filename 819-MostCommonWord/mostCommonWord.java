4c2b63ffbf3df282334f291a920241128df8f89a
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-08-19 21:03:49 -0700

    Update mostCommonWord.java

--- 819-MostCommonWord/mostCommonWord.java
+++ 819-MostCommonWord/mostCommonWord.java
@@ -2,8 +2,9 @@ class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
         Set<String> banSet = new HashSet<>(Arrays.asList(banned));
         Map<String, Integer> countMap = new HashMap<>();
-        //String[] words = paragraph.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");   // \s whitespace 
-        String[] words = paragraph.replaceAll("\\p{P}", "").toLowerCase().split("\\s+");
+        // \p{Punct}, Punctuation: One of !"#$%&'()*+,-./:;<=>?@[]^_`{|}~          
+        // \s whitespace 
+        String[] words = paragraph.replaceAll("\\p{Punct}", " ").toLowerCase().split("\\s+");
         for(String w: words) {
             if (!banSet.contains(w))
                 countMap.put(w, countMap.getOrDefault(w, 0) + 1);
