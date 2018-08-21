8bbc5a571908919280743dee07253843ffabcf36
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-08-21 13:03:48 -0700

    Update copyRandomList.java

--- 138-copyRandomList/copyRandomList.java
+++ 138-copyRandomList/copyRandomList.java
@@ -9,16 +9,18 @@
 public class Solution {
     public RandomListNode copyRandomList(RandomListNode head) {
         if (head == null) return null;
-        Map<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();
+        // Map<originalNode, copiedNode>
+        Map<RandomListNode, RandomListNode> map = new HashMap<RandomListNode, RandomListNode>();  
         RandomListNode curr = head;
         
-        // 1st loop copies all nodes with labels, map <inputNode, copiedNode>
+        // 1st loop make copies of all nodes and their labels, and maps each orignal node with its copy  
         while(curr != null) {
             map.put(curr, new RandomListNode(curr.label));
             curr = curr.next;
         }
         curr = head;
-        // 2nd loop connects all nodes with .next and .random.   get<Key> = <Value> ---> get<inputNode> = copiedNode
+        // 2nd loop connects all nodes with .next and .random.   
+        // get<Key> = <Value> ---> get<originalNode> = copiedNode
         while(curr != null) {
             map.get(curr).next = map.get(curr.next);
             map.get(curr).random = map.get(curr.random);
