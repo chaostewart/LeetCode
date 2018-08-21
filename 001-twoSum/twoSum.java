bdd933aa46d4c7791310ce5e56307530f7d2b0c3
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-08-21 13:20:58 -0700

    Update twoSum.java

--- 001-twoSum/twoSum.java
+++ 001-twoSum/twoSum.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // Map<intValue, intIndex>
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
             int complement = target - nums[i];  // check if the current number's complement is already in the map
@@ -8,6 +9,7 @@ class Solution {
             }
             map.put(nums[i], i);  // if not, map the current number and its index
         }
+        //return new int[2];  // requires a return statement
         throw new IllegalArgumentException("No solution");
     }
 }
