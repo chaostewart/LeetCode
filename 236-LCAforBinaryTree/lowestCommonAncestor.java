00d0ad7425bf68d2a8d9a003093192a9c3dfcd78
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-08-22 11:48:10 -0700

    Update lowestCommonAncestor.java

--- 236-LCAforBinaryTree/lowestCommonAncestor.java
+++ 236-LCAforBinaryTree/lowestCommonAncestor.java
@@ -7,15 +7,21 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+/*
+If the current (sub)tree contains both p and q, then the function result is their LCA. 
+####### If only one of them is in that subtree, then the result is that one of them. ######
+If neither are in that subtree, the result is null.
+*/
+
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // has to be recursive
-        if (root == null || root == p || root == q)    // reached the leaves or found p or q
+        if (root == null || root == p || root == q)    // tree is empty OR reached a leaf's child OR found either p or q
             return root;
         TreeNode left = lowestCommonAncestor(root.left, p, q);     // recurse on the left subtree
         TreeNode right = lowestCommonAncestor(root.right, p, q);   // recurse on the right subtree
-        if (left != null && right != null)     // if both children are not null, current root is LCA
+        if (left != null && right != null)     // if both nodes are found, current root is LCA
             return root;
-        return left != null ? left : right;   // return to the upper level
+        return left != null ? left : right;   // if only one node is found in the tree, then that node is the LCA
     }
 }
