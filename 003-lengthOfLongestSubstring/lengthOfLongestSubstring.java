4293b6db792536dc8b2b2e56ee77677b85ce09dc
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-08-22 00:30:55 -0700

    Update lengthOfLongestSubstring.java

--- 003-lengthOfLongestSubstring/lengthOfLongestSubstring.java
+++ 003-lengthOfLongestSubstring/lengthOfLongestSubstring.java
@@ -3,16 +3,18 @@ class Solution {
         if (s.length() == 0 || s.length() == 1) {  // special simple cases
             return s.length();
         }
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        int max = 0;
-        for (int i = 0, j = 0; i < s.length(); i++) { //two pointers would only move to the right
+        // Map<char, char's last index in string>
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();       
+        int res = 0;
+        //create two pointers, i moves along the string, j points at the begining of a substring
+        // both pointers move towards the same direction
+        for (int i = 0, j = 0; i < s.length(); i++) { 
             if (map.containsKey(s.charAt(i))) {
                 j = Math.max(j, map.get(s.charAt(i)) + 1);  // use Math.max here to avoid j rolling backwards
             }
-            map.put(s.charAt(i), i);   // replace recurring char's value with the latest index
-            max = Math.max(max, i - j + 1);  // substring's indces are continuous ints
-            
+            map.put(s.charAt(i), i);   // insert a new char or replace recurring char's value with the latest index
+            res = Math.max(res, i - j + 1); 
         }
-        return max;
+        return res;
     }
 }
