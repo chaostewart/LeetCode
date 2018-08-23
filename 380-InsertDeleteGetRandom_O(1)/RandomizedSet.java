414042f902a16cc92c3096e91b40fcfa3f644f78
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-08-23 15:13:47 -0700

    Update RandomizedSet.java

--- 380-InsertDeleteGetRandom_O(1)/RandomizedSet.java
+++ 380-InsertDeleteGetRandom_O(1)/RandomizedSet.java
@@ -1,26 +1,28 @@
 class RandomizedSet {
     List<Integer> nums;               // declare private variables
     Map<Integer, Integer> map;
-    java.util.Random rand = new Random();    /// but rand needs to be initialized here
+    java.util.Random rand;  
     /** Initialize your data structure here. */
+    
     public RandomizedSet() {
         nums = new ArrayList<>();
-        map = new HashMap<>();       // map<nums.get(i), i>   -> <value, index>
+        map = new HashMap<>();       //  Map<nums.get(i), i>  ===>> <value, index>
+        rand = new Random();
     }
     
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
-    public boolean insert(int val) {
-        boolean contain = map.containsKey(val);   
-        if (contain) return false;
-        map.put(val, nums.size());      // add to ArrayList and HashMap
+    public boolean insert(int val) {  
+        if (map.containsKey(val)) 
+            return false;
+        map.put(val, nums.size());      // add to HashMap then ArrayList
         nums.add(val);
         return true;
     }
     
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     public boolean remove(int val) {
-        boolean contain = map.containsKey(val);
-        if (!contain) return false;
+        if (!map.containsKey(val)) 
+            return false;
         int index = map.get(val);
         if (index != nums.size() - 1) {     // if val to remove is not the last elem, need to do SWAP first
             int lastVal = nums.get(nums.size() - 1);
@@ -28,7 +30,7 @@ class RandomizedSet {
             nums.set(index, lastVal);
         }
         map.remove(val);                // remove the last elem in nums from ArrayList and HashMap
-        nums.remove(nums.size() - 1);      
+        nums.remove(nums.size() - 1);      // ArrayList is a dynamic structure, size can change
         return true;
     }
     
