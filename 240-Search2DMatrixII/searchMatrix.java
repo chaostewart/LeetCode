754216af0c01cd5582e1b05ac2bbd4d3437b1ae0
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-08-22 17:04:24 -0700

    Update searchMatrix.java

--- 240-Search2DMatrixII/searchMatrix.java
+++ 240-Search2DMatrixII/searchMatrix.java
@@ -2,6 +2,8 @@ class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix == null || matrix.length < 1 || matrix[0].length < 1) return false;
         // This genius algo. starts the search from top right!!  Time complexity is O(m + n)
+        // traversing a row in descending order rules out other possibilities of finding target in a row
+        // starting the search of target from bottom left would work too
         int row = 0, col = matrix[0].length - 1;
         while (row < matrix.length && col >= 0) {
             if (matrix[row][col] == target) 
@@ -11,6 +13,6 @@ class Solution {
             else       // move downwards if target > current number
                 row++;
         }
-        return false;
+        return false;   // while loop condition cannot be met anymore, fail to find target
     }
 }
