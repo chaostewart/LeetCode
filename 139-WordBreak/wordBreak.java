1abda8f3779042361f812fac3afa3f98b7b3ebf9
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-08-25 17:36:04 -0700

    Improved time complexity, same as Coin Change prob

--- 139-WordBreak/wordBreak.java
+++ 139-WordBreak/wordBreak.java
@@ -2,20 +2,21 @@ class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
         // 1st Dynamic Programming problem!  Instantiate a DP array!
         boolean[] flag = new boolean[s.length() + 1];
-        flag[0] = true;   // first elem is like a dummy
+        flag[0] = true;   // first elem acts like a dummy
         for (int i = 1; i <= s.length(); i++) {   
-            for (int j = 0; j < i; j++) {
+            for (String w: wordDict) {
                 /* if in string s.substring(0, j) can be segamented into a space-separated sequence
                 *                                         AND s.substring(j, i) is a word in given wordDict, 
                 * then s.substring(0, i) can be segamented into a space-separated sequence, 
                 *                                         which is indicated by flag[i]
                 */
-                if (flag[j] && wordDict.contains(s.substring(j, i))) {  
+                int len = w.length();
+                if (i >= len && flag[i - len] && s.substring(i - len, i).equals(w)) {  
                     flag[i] = true;
                     break;
                 }
             }
         }
-        return flag[s.length()];   // flag[last elem] is the answer  
+        return flag[s.length()];   // flag[lastElement] is the answer  
     }
 }
