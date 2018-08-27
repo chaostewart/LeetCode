e00959bfd136ba3b69e1634f049db3503d67f241
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-08-27 13:36:21 -0700

    Update boundaryOfBinaryTree.java

--- 545-BoundaryOfBinaryTree/boundaryOfBinaryTree.java
+++ 545-BoundaryOfBinaryTree/boundaryOfBinaryTree.java
@@ -8,41 +8,39 @@
  * }
  */
 class Solution {
-    List<Integer> ans = new ArrayList<>(1000);
     public List<Integer> boundaryOfBinaryTree(TreeNode root) {
+        List<Integer> ans = new ArrayList<>(1000);
         if (root == null) return ans;
         ans.add(root.val);
         if (root.left == null && root.right == null) // the case where the tree has only one node
             return ans;
-        getLeftBoundary(root.left);     // excluding the left-most node which is also a leaf
-        getLeaves(root);                // including both left-most and right-most leaves
-        getRightBoundary(root.right);   // excluding the right-most node
+        getLeftBoundary(root.left, ans);     // excluding the left-most node which is also a leaf
+        getLeaves(root, ans);                // including both left-most and right-most leaves
+        getRightBoundary(root.right, ans);   // excluding the right-most node
         return ans;
     }
     
-    private void getLeftBoundary(TreeNode root) {
+    private void getLeftBoundary(TreeNode root, List<Integer> ans) {
         // if original root's left child is null, or reached the left-most node, stop recursion
         if (root == null || (root.left == null && root.right == null)) return;    
         ans.add(root.val);    // Note: add value before recursion
-        if (root.left != null) getLeftBoundary(root.left);   
-        else getLeftBoundary(root.right);   // if left subtree is null, traverse right subtree
+        getLeftBoundary(root.left == null ? root.right : root.left, res);// if left subtree is null, traverse right subtree
     }
     
-    private void getRightBoundary(TreeNode root) {
+    private void getRightBoundary(TreeNode root, List<Integer> ans) {
         // if original root's right child is null, or reached the right-most node, stop recursion
         if (root == null || (root.left == null && root.right == null)) return;                
-        if (root.right != null) getRightBoundary(root.right);   
-        else getRightBoundary(root.left);  // if right subtree is null, traverse left subtree
+        getRightBoundary(root.right == null ? root.left : root.right, res);// if right subtree is null, traverse left subtree
         ans.add(root.val);    // Note: add value after recursion to get reversed nodes
     }
     
-    private void getLeaves(TreeNode root) {
+    private void getLeaves(TreeNode root, List<Integer> ans) {
         if (root.left == null && root.right == null) {    // reached leaves, add value, return
             ans.add(root.val);
             return;
         }
-        if (root.left != null) getLeaves(root.left);     // traverse internal nodes from left to right
-        if (root.right != null) getLeaves(root.right);       
+        if (root.left != null) getLeaves(root.left, ans);     // traverse internal nodes from left to right
+        if (root.right != null) getLeaves(root.right, ans);       
     }
     
 }
