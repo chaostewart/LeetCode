32a7037ae699e0a7e4f0375db0bdfce9f580aff8
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-08-27 17:40:50 -0700

    Update and rename 238-productExceptSelf/productExceptSelf.java to 238-ProductExceptSelf/productExceptSelf.java

--- 238-ProductExceptSelf/productExceptSelf.java
+++ 238-ProductExceptSelf/productExceptSelf.java
@@ -4,15 +4,16 @@ class Solution {
         int[] output = new int[len];   
         output[0] = 1;   // initilize 1st output elem to be 1
         // the following loop calculates the product of all elems on the left side of each elem i
-        for (int i = 1; i < len; i++) {
+        for (int i = 1; i < len; i++) 
             output[i] = output[i-1] * nums[i-1];
-        }
+        
+        // the following loop then multiplies the product of all elems on the right side of each elem i
         int right = 1;
-        // the following loop then includes the product of all elems on the right side of each elem i
         for (int j = len - 1; j >= 0; j--) {
             output[j] *= right;
             right *= nums[j];
         }
+        
         return output;
     }
 }
