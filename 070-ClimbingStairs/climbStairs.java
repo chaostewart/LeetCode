7bfeabfee50b7d0f00771a0cfd521312f2a0cceb
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-08-28 19:24:43 -0700

    Update climbStairs.java

--- 070-ClimbingStairs/climbStairs.java
+++ 070-ClimbingStairs/climbStairs.java
@@ -1,11 +1,9 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n <=0 ) return 0; // the number of ways should be zero.
         if (n == 1)  return 1;  // there is only way to climb the stair.
         if (n == 2)  return 2;  // there are two ways to climb the stairs. 
         
         int[] dp = new int[n + 1];
-        dp[0] = 0;
         dp[1] = 1;
         dp[2] = 2;
         for (int i = 3; i <= n; i++) 
