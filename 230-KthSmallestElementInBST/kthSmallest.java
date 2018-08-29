cfad69addba00d96647ee57acc468929fedfd84a
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-08-29 12:05:42 -0700

    Add files via upload

---
+++ 230-KthSmallestElementInBST/kthSmallest.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    // sol1: binary search, get subtree size, determine where answer resides
+    public int kthSmallest(TreeNode root, int k) {
+        int count = countNodes(root.left);
+        if (k <= count)                        // left subtree has >= k nodes, ans is in the left subtree
+            return kthSmallest(root.left, k);
+        else if (k > count + 1)                // ans is in the right subtree
+            return kthSmallest(root.right, k - (1 + count));    // 1 is counted as current node
+        
+        // k == count + 1, ans is the current root node
+        return root.val;
+    }
+    
+    public int countNodes(TreeNode n) {
+        if (n == null) return 0;       
+        return 1 + countNodes(n.left) + countNodes(n.right);
+    }
+    
+    /*
+    // sol 2: in-order traverse the BST, same run time as above
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> nums = new ArrayList<>();
+        inorder(root, nums, k);
+        return nums.get(k-1);
+    }
+    
+    private void inorder(TreeNode root, List<Integer> nums, int k) {
+        if (root == null || nums.size() == k) return;
+        inorder(root.left, nums, k);
+        nums.add(root.val);
+        inorder(root.right, nums, k);
+    }
+    */
+}
