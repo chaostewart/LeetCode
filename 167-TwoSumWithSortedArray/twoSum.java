ba2da9f5db44aa90e3b092d417113d7133a328a6
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-08-29 22:43:19 -0700

    Update twoSum.java

--- 167-TwoSumWithSortedArray/twoSum.java
+++ 167-TwoSumWithSortedArray/twoSum.java
@@ -4,7 +4,7 @@ class Solution {
         int[] res = new int[2];
         if (numbers == null || numbers.length < 2) return res;
         int left = 0, right = numbers.length - 1;
-        while (left < right) {   // keep moving two pointers towards each other
+        while (left < right) {   // keep moving two pointers towards each other, may not use the same element twice
             if (numbers[left] + numbers[right] == target) {
                 res[0] = ++left; res[1] = ++right;   // answers are not zero-based
                 return res;
