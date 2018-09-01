5a3b76bbd2c96753dadc299078aaf9237df8e78d
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-09-01 16:25:40 -0700

    Update findKthLargest.java

--- 215-KthLargestElementInArray/findKthLargest.java
+++ 215-KthLargestElementInArray/findKthLargest.java
@@ -1,7 +1,7 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
         // Slow sol1: i came up with this solution because i just learnt how to implement P.Q. ....
-        // O(Nlgk) running time + O(k) memory
+        // O(Nlogk) running time + O(k) memory
         /* 
         PriorityQueue<Integer> heap = new PriorityQueue<>();  // Natural order so don't need (num1, num2) -> num1 - num2 
         for (int i: nums) {
@@ -11,7 +11,7 @@ class Solution {
         }
         return heap.poll();
          */
-        // Fast sol2: O(N lg N) running time + O(1) memory
+        // Fast sol2: O(NlogN) running time + O(1) memory
         Arrays.sort(nums);
         return nums[nums.length - k];
     }
