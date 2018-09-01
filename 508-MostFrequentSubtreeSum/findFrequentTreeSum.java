13cdfef51ae8589ae37dbfa2d270397ef1c82a8a
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-09-01 15:34:01 -0700

    Update findFrequentTreeSum.java

--- 508-MostFrequentSubtreeSum/findFrequentTreeSum.java
+++ 508-MostFrequentSubtreeSum/findFrequentTreeSum.java
@@ -7,31 +7,30 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
 class Solution {
-    int max_freq;   // records the max frequency of a sum
-    Map<Integer, Integer> map;
+    private int max_freq;   // records the max frequency of a sum
+    
     public int[] findFrequentTreeSum(TreeNode root) {
         max_freq = 0;
-        map = new HashMap<>();    // map<subtree sum value, frequency>
-        dfsGetSum(root);
+        Map<Integer, Integer> map = new HashMap<>();    // map<subtree sum value, frequency>
+        dfsGetSum(root, map);
         List<Integer> ans = new ArrayList<>();     // store sums with max frequency in a list
         
-        for (int val: map.keySet()) {
-            if (map.get(val) == max_freq) {
+        for (int val: map.keySet())
+            if (map.get(val) == max_freq)
                 ans.add(val);
-            }
-        }
         
         int[] res = new int[ans.size()];   // convert list to array
-        for (int i = 0; i < ans.size(); i++) {
+        for (int i = 0; i < ans.size(); i++) 
             res[i] = ans.get(i);
-        }
+        
         return res;
     }
     
-    private int dfsGetSum(TreeNode root) {
+    private int dfsGetSum(TreeNode root, Map<Integer, Integer> map) {
         if (root == null) return 0;   // null returns 0
-        int sum = dfsGetSum(root.left) + dfsGetSum(root.right) + root.val;    // left subtree + right subtree + root val
+        int sum = dfsGetSum(root.left, map) + dfsGetSum(root.right, map) + root.val;    // left subtree + right subtree + root val
         map.put(sum, map.getOrDefault(sum, 0) + 1);    
         max_freq = Math.max(max_freq, map.get(sum));
         return sum;
