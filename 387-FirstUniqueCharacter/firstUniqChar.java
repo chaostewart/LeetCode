5568931960b5552657674e05aceeed300662fe24
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-09-02 21:04:21 -0700

    Update firstUniqChar.java

--- 387-FirstUniqueCharacter/firstUniqChar.java
+++ 387-FirstUniqueCharacter/firstUniqChar.java
@@ -1,21 +1,7 @@
 class Solution {
     public int firstUniqChar(String s) {
         if (s.isEmpty()) return -1;
-        // Snail sol1: no need to complicate the prob!
-        /*
-        Map<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (map.containsKey(c)) 
-                map.put(c, s.length());
-            else
-                map.put(c, i);
-        }
-        int min = Collections.min(map.entrySet(), Map.Entry.comparingByValue()).getValue();
-        return min == s.length() ? -1 : min;
-        */
-        
-        // Rocket sol2: simple hash table
+        // simple hash table
         int[] freq = new int[26]; 
         for (int i = 0; i < s.length(); i++) {
             freq[s.charAt(i) - 'a']++;
