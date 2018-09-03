5a8fd638d0e4d65af3de73fce85d4e8abd705bbd
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-09-02 21:16:59 -0700

    Update maxProfit.java

--- 121-maxProfit/maxProfit.java
+++ 121-maxProfit/maxProfit.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int min = Integer.MAX_VALUE;
-        int maxProfit = 0;
-        for (int i = 0; i < prices.length; i++) {
-            if (prices[i] < min) 
-                min = prices[i];       // find "valleys"
-            else if (prices[i] - min > maxProfit) 
-                maxProfit = prices[i] - min;    // calculate if a following peak - a valley gives the max
+        int res = 0;
+        for (int p : prices) {
+            if (p < min)
+                min = p;    // find "valleys"
+            else if (p - min > res)
+                res = p - min;   // calculate if the following peak - valley gives the max
         }
-        return maxProfit;
+        return res;
     }
 }
