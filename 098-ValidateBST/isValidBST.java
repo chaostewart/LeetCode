0b512d5876858b2ff5122a0a19a5d28491e42558
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-09-05 15:13:41 -0700

    Update isValidBST.java

--- 098-ValidateBST/isValidBST.java
+++ 098-ValidateBST/isValidBST.java
@@ -11,17 +11,17 @@ class Solution {
     public boolean isValidBST(TreeNode root) {
         // Using Stack to do an inorder traversal, note all nodes are touched from the smallest value to the largest
         if (root == null) return true;
-        TreeNode prev = null;
+        TreeNode prev = null, curr = root;
         Stack<TreeNode> stack = new Stack<>();
-        while (root != null || !stack.isEmpty()) { // if root is not a right child of leaf node or not all nodes have been touched
-            while (root != null) {    // if root is not a right child of leaf node 
-                stack.push(root);     // push all left broundary of a subtree to the stack
-                root = root.left;
+        while (curr != null || !stack.isEmpty()) { // if root is not a right child of leaf node OR not all nodes have been touched
+            while (curr != null) {    // if root is not a right child of leaf node 
+                stack.push(curr);     // push all left broundary of a subtree to the stack
+                curr = curr.left;
             }
-            root = stack.pop();
-            if (prev != null && prev.val >= root.val) return false;   // prev only equals null at initialization
-            prev = root;
-            root = root.right;  // in the next round, a node will be compared to its right child
+            curr = stack.pop();
+            if (prev != null && prev.val >= curr.val) return false;   // prev only equals null at initialization
+            prev = curr;
+            curr = curr.right;  // in the next round, a node will be compared to its right child
         }
         return true;
     }
