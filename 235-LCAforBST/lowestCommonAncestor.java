31bd582d8d7aa247d12bfb0445fc3f0c9067eea3
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-09-06 16:56:12 -0700

    Update lowestCommonAncestor.java

--- 235-LCAforBST/lowestCommonAncestor.java
+++ 235-LCAforBST/lowestCommonAncestor.java
@@ -11,9 +11,8 @@ class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         // this is not just a binary tree...a BST has its left child smaller than it, and right child larger than it
         // here comes a genius's code...OMG..
-        while((root.val - p.val) * (root.val - q.val) > 0) {
-            root = root.val > p.val ? root.left : root.right;
-        }
+        while((root.val - p.val) * (root.val - q.val) > 0)   // keep searching when p & q are on the same side of root
+            root = root.val > p.val ? root.left : root.right;      
         return root;
     }
 }
