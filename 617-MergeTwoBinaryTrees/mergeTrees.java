2ae1f7b41d47c34ea8ea894b7cda82263b6ce9a6
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-09-06 17:30:21 -0700

    Update mergeTrees.java

--- 617-MergeTwoBinaryTrees/mergeTrees.java
+++ 617-MergeTwoBinaryTrees/mergeTrees.java
@@ -8,32 +8,14 @@
  * }
  */
 class Solution {
-    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        if (t1 == null && t2 == null) return null; 
-        TreeNode tsum = new TreeNode(0);  // need to initialize a value
-        if (t1 != null && t2 != null){    // if both nodes are not null, sum the vals
-            tsum.val = t1.val + t2.val;
-            tsum.left = mergeTrees(t1.left, t2.left);    // do it recursively on both children
-            tsum.right = mergeTrees(t1.right, t2.right);
-        }
-        else
-            tsum = t1 == null ? t2 : t1;  // tsum copies the not null node
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {    
+        if (t1 == null || t2 == null) 
+            return t1 == null ? t2 : t1; 
+        // must be the case where neither node is null
+        TreeNode tsum = new TreeNode(t1.val + t2.val);  // need to initialize the value as sum
+        tsum.left = mergeTrees(t1.left, t2.left);    // do it recursively on both children
+        tsum.right = mergeTrees(t1.right, t2.right);
+
         return tsum;
     }
 }
-
-/* more concise version
-public class Solution {
-    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
-        if (t1 == null && t2 == null) return null;
-        
-        int val = (t1 == null ? 0 : t1.val) + (t2 == null ? 0 : t2.val);
-        TreeNode newNode = new TreeNode(val);
-        
-        newNode.left = mergeTrees(t1 == null ? null : t1.left, t2 == null ? null : t2.left);
-        newNode.right = mergeTrees(t1 == null ? null : t1.right, t2 == null ? null : t2.right);
-        
-        return newNode;
-    }
-}
-*/
