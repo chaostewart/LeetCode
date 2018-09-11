e4e92fbc47178b6f07bd5816cc4a82abe0e27210
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-09-10 20:26:35 -0700

    Update countPrimes.java

--- 204-CountPrimes/countPrimes.java
+++ 204-CountPrimes/countPrimes.java
@@ -2,18 +2,17 @@ class Solution {
     public int countPrimes(int n) {
         if (n < 2) return 0;    // corner cases
         boolean[] notPrime = new boolean[n];
-        notPrime[0] = true; notPrime[1] = true;   // for easing counting later
+        notPrime[0] = true; notPrime[1] = true;   // for easy counting later
         
-        for (int i = 2; i < n; i++) {
-            if (!notPrime[i]){    // if current int is a prime
-                for (int j = 2; i*j < n; j++) {
-                    notPrime[i*j] = true;    // label more non prime ints
-                }
-            }
-        }
+        for (int i = 2; i < n; i++)
+            if (!notPrime[i])    // if current int is a prime
+                for (int j = 2; i * j < n; j++)     
+                    notPrime[i * j] = true;    // label more non prime ints
         
         int count = 0;
-        for (boolean k: notPrime) if (!k) count++;
+        for (boolean k: notPrime) 
+            if (!k) 
+                count++;
         return count;
     }
 }
