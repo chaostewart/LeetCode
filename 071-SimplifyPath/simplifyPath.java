4bb1f1b556ddbeb96f7c81e355fa9fc2c0321694
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-09-13 12:10:55 -0700

    Add files via upload

---
+++ 071-SimplifyPath/simplifyPath.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+        Set<String> skip = new HashSet<>(Arrays.asList("..", ".", ""));
+        
+        for (String dir : path.split("/")) {
+            if (dir.equals("..") && !stack.isEmpty()) 
+                stack.pop();
+            else if (!skip.contains(dir)) 
+                stack.push(dir);
+        }
+        
+        String res = "";
+        while(!stack.isEmpty()) 
+            res = "/" + stack.pop() + res;
+        return res.isEmpty() ? "/" : res;
+    }
+}
