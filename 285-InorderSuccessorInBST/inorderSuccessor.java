52911f7d0c8f817370d44798ddca5455d0952b69
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-09-15 19:29:49 -0700

    Add files via upload

---
+++ 285-InorderSuccessorInBST/inorderSuccessor.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/*
+That node can either be p's parent or the smallest node in p' right branch.
+When the code runs into the else block, that means the current root is either p's parent or a node in p's right branch.
+ 
+If it's p's parent node, there are two scenarios: 
+1. p doesn't have right child, in this case, the recursion will eventually return null, so p's parent is the successor; 
+2. p has right child, then the recursion will return the smallest node in the right sub tree, and that will be the answer.
+
+If it's p's right child, there are two scenarios: 
+2a. the right child has left sub tree, eventually the smallest node from the left sub tree will be the answer; 
+2b. the right child has no left sub tree, the recursion will return null, then the right child (root) is our answer.
+*/
+class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if (root == null)
+            return null;
+
+        if (root.val <= p.val)                  
+            return inorderSuccessor(root.right, p);
+        else {                                 
+            TreeNode left = inorderSuccessor(root.left, p);
+            return (left != null) ? left : root;   
+        }
+    }
+}
