905f59634e8b44c7834b4ccb96c690523685d0c5
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-09-24 19:31:21 -0700

    Add files via upload

---
+++ 110-BalancedBinaryTree/isBalanced.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        int heightL = getHeight(root.left);
+        int heightR = getHeight(root.right);
+        return isBalanced(root.left) && isBalanced(root.right) && Math.abs(heightL - heightR) <= 1;
+    }
+    
+    private int getHeight(TreeNode node) {
+        if (node == null) return 0;
+        return Math.max(getHeight(node.left), getHeight(node.right)) + 1;
+    }
+}
