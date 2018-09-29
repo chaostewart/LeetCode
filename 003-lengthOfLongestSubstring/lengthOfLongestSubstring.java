ff9ecd35710a643a7893b382e6fe1dd8a75dcc48
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-09-29 12:10:02 -0700

    Update lengthOfLongestSubstring.java

--- 003-lengthOfLongestSubstring/lengthOfLongestSubstring.java
+++ 003-lengthOfLongestSubstring/lengthOfLongestSubstring.java
@@ -1,20 +1,21 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() == 0 || s.length() == 1) {  // special simple cases
-            return s.length();
+        if (s.length() == 0) return 0;
+        int ans = 0;
+        // Map<char, char's most recent position>
+        Map<Character, Integer> map = new HashMap<>();
+        int start = 0, end = 0;
+        // create two pointers, end moves along the string, 
+        // start points at the begining of the next substring
+        // both pointers only move towards right
+        while (end < s.length()) {
+            char curr = s.charAt(end);
+            if (map.containsKey(curr))
+                start = Math.max(start, map.get(curr) + 1);
+            map.put(curr, end);
+            ans = Math.max(ans, end - start + 1);
+            end++;
         }
-        // Map<char, char's last index in string>
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();       
-        int res = 0;
-        //create two pointers, i moves along the string, j points at the begining of the next substring without repeating char
-        // both pointers move towards the same direction
-        for (int i = 0, j = 0; i < s.length(); i++) { 
-            if (map.containsKey(s.charAt(i))) {
-                j = Math.max(j, map.get(s.charAt(i)) + 1);  // use Math.max here to avoid j rolling backwards
-            }
-            map.put(s.charAt(i), i);   // insert a new char or replace recurring char's value with the latest index
-            res = Math.max(res, i - j + 1); 
-        }
-        return res;
+        return ans;
     }
 }
