ffd6e899bd89ade3fbdc11dd49a52458c3922003
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-04 16:00:11 -0700

    Update myPow.java

--- 050-Power(x,n)/myPow.java
+++ 050-Power(x,n)/myPow.java
@@ -1,7 +1,7 @@
 class Solution {
     public double myPow(double x, int n) {
         // Integer.MAX_VALUE = 2147483647, Integer.MIN_VALUE = -2147483648
-        if(n < 0) return 1/x * myPow(1/x, - (n + 1));
+        if(n < 0) return 1/x * myPow(1/x, - (n + 1));   // this opereation avoids overflow
         if(n == 0) return 1;
         if(n == 2) return x * x;
         if(n % 2 == 0) return myPow(myPow(x, n/2), 2);
