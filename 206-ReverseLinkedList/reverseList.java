ffb028c96ce50d6216a6f1461ba0c072f9e8e3d7
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-04 18:25:35 -0700

    Update reverseList.java

--- 206-ReverseLinkedList/reverseList.java
+++ 206-ReverseLinkedList/reverseList.java
@@ -24,10 +24,13 @@ class Solution {
 
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if (head == null || head.next == null) // if it's an empty list or it has reached the last listnode
-            return head; //return the curr pointer
-        ListNode p = reverseList(head.next);    // otherwise, reverse the rest of the linked list
-        //after the rest has been reversed, it looks like ...-->node_n(head)-->node_n+1(head.next)<--node_n+2<--node_n+3...
+        // if it's an empty list or it has reached the last node in list
+        if (head == null || head.next == null) 
+            return head; //return the curr node
+        // otherwise, reverse the rest of the linked list, the returned p is the last node in list
+        ListNode p = reverseList(head.next);    
+        //after the rest has been reversed, it looks like ...-->node_i(head)-->node_i+1(head.next)<--node_i+2<--node_i+3...
+        // and node_i+1's next points to NULL
         head.next.next = head;   
         head.next = null;
         return p;
