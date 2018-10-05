0364d8d65d6d5b5ac50799edf394594f59e0365e
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-04 19:13:20 -0700

    Update groupAnagrams.java

--- 049-GroupAnagrams/groupAnagrams.java
+++ 049-GroupAnagrams/groupAnagrams.java
@@ -1,16 +1,20 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs == null || strs.length == 0) 
-            return new ArrayList<List<String>>();   // don't forget the special case..
-        Map<String, List<String>> map = new HashMap<>(); // Map a string (sorted charArray), to ArrayList<String>
-        for (String s : strs) {  // exam each string in strs
-            char[] chars = s.toCharArray();    // these 3 steps sort the chars in string s
-            Arrays.sort(chars);
-            String strKey = String.valueOf(chars);
-            if (!map.containsKey(strKey))      // if it's the 1st appearance of an anagram
-                map.put(strKey, new ArrayList<String>());   // creat a new string list for this group
-            map.get(strKey).add(s);    // add string s (before sort)  to the list
+        List<List<String>> res = new ArrayList<>();
+        Map<String, List<String>> map = new HashMap<>();  // Map a string (sorted charArray), to ArrayList<String>
+        for (String s : strs) {
+            char[] chs = s.toCharArray();
+            Arrays.sort(chs);
+            String tmp = String.valueOf(chs);
+            if (!map.containsKey(tmp))
+                map.put(tmp, new ArrayList<String>());
+            map.get(tmp).add(s);
         }
-        return new ArrayList<List<String>>(map.values());    // map.values() return a collection of view of values...
+        /*
+        for (String s : map.keySet())
+            res.add(map.get(s));
+        return res;
+        */
+        return new ArrayList<>(map.values());    // map.values() return a collection of view of values...
     }
 }
