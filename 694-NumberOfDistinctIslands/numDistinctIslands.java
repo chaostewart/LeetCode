65a5e93040aeb2823415e67f0bb13d869d5a3dfd
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-09 13:09:18 -0700

    Update numDistinctIslands.java

--- 694-NumberOfDistinctIslands/numDistinctIslands.java
+++ 694-NumberOfDistinctIslands/numDistinctIslands.java
@@ -1,35 +1,27 @@
 class Solution {
-    private int[][] grid;
-    private boolean[][] seen;
-    private List<Integer> shape;   // declare global variables so that private helper function can mutate them
-    
     public int numDistinctIslands(int[][] grid) {
-        this.grid = grid;
-        seen = new boolean[grid.length][grid[0].length];    // record if a square has been touched by DFS
-        Set<List<Integer>> shapeSet = new HashSet<>();      // shapeSet stores distinct island shapes
-        
-        for (int row = 0; row < grid.length; row++) {
-            for (int col = 0; col < grid[0].length; col++) {
-                if (grid[row][col] == 1 && !seen[row][col]) {
-                    shape = new ArrayList<>();              // records the next island's shape/DFS path
-                    DFS(row, col, 0);
-                    if (!shape.isEmpty())
-                        shapeSet.add(shape);         // if the shape of island is unseen, this new shape will be added to set
+        boolean[][] visited = new boolean[grid.length][grid[0].length];   // record if a square has been visited
+        Set<List<Integer>> shapeSet = new HashSet<>();   // shapeSet stores distinct island shapes
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid[0].length; j++)
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                    List<Integer> path = new ArrayList<>();    // records the next island's shape/DFS path
+                    DFS(grid, visited, path, 0, i, j);
+                    if (!path.isEmpty())     // if the shape of island is unseen, this new shape will be added to set
+                        shapeSet.add(path);
                 }
-            }
-        }
         return shapeSet.size();
     }
     
-    private void DFS(int r, int c, int path) {
-        if (r >= 0 && r < grid.length && c >= 0 && c < grid[0].length && grid[r][c] == 1 && !seen[r][c]) {
-            shape.add(path);
-            seen[r][c] = true;
-            DFS(r - 1, c, 1);   // up
-            DFS(r, c + 1, 2);   // right
-            DFS(r + 1, c, 3);   // down
-            DFS(r, c - 1, 4);   // left
-            shape.add(0);       // start and end of a path are labelled as '0'
+    private void DFS(int[][] grid, boolean[][] visited, List<Integer> path, int dir, int i, int j) {
+        if (i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == 1 && !visited[i][j]) {
+            visited[i][j] = true;
+            path.add(dir);
+            DFS(grid, visited, path, 1, i + 1, j);
+            DFS(grid, visited, path, 2, i - 1, j);
+            DFS(grid, visited, path, 3, i, j + 1);
+            DFS(grid, visited, path, 4, i, j - 1);
+            path.add(0);    // start and end of a path are labelled as '0'
         }
     }
 }
