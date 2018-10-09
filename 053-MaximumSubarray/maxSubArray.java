4cf323f8f396b6fe2acee6638f746e46ab320418
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-09 12:25:41 -0700

    Update maxSubArray.java

--- 053-MaximumSubarray/maxSubArray.java
+++ 053-MaximumSubarray/maxSubArray.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int n = nums.length;
-        int[] dp = new int[n];     //dp[i] means the maximum subarray ending with A[i];
+        int[] dp = new int[nums.length];     //dp[i] means the maximum subarray ending with nums[i];
         dp[0] = nums[0];
         int res= dp[0];
         
-        for(int i = 1; i < n; i++){
+        for(int i = 1; i < nums.length; i++){
+            // if previous subarray has sum larger than 0, then attach the current to previous
             dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
-            res = Math.max(res dp[i]);
+            res = Math.max(res, dp[i]);
         }
         
         return res;
