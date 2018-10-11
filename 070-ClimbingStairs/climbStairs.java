2eb751f86f0508864714a338ff06fc3dbc96fe68
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-10 19:01:39 -0700

    Update climbStairs.java

--- 070-ClimbingStairs/climbStairs.java
+++ 070-ClimbingStairs/climbStairs.java
@@ -1,3 +1,16 @@
+// turns out there's no special test cases such as n = 0 or 1 or 2..
+class Solution {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++)
+            dp[i] = dp[i - 1] + dp[i - 2];
+        return dp[n];
+    }
+}
+
+
 class Solution {
     public int climbStairs(int n) {
         if (n == 1)  return 1;  // there is only way to climb the stair.
