d087559a6c73d60a1de774e3ac820df45b207654
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-10 18:20:12 -0700

    Update calPoints.java

--- 682-BaseballGame/calPoints.java
+++ 682-BaseballGame/calPoints.java
@@ -1,26 +1,22 @@
 class Solution {
     public int calPoints(String[] ops) {
-        // Use a stack because we at most need to trace back the last two valid scores
-        // push and pop are easy
         Stack<Integer> stack = new Stack<>();
-        for (String op: ops) {
-            if (op.equals("+")) {
-                int top = stack.pop();
-                int newTop = top + stack.peek();
-                stack.push(top);
-                stack.push(newTop);
-            } else if (op.equals("D")) {
-                stack.push(2 * stack.peek());
-            } else if (op.equals("C")) {
+        for (String s : ops) {
+            if (s.equals("+")) {
+                int prev = stack.pop();
+                int curr = prev + stack.peek();
+                stack.push(prev);
+                stack.push(curr);
+            } else if (s.equals("D"))
+                stack.push(2*stack.peek());
+            else if (s.equals("C"))
                 stack.pop();
-            } else {
-                stack.push(Integer.valueOf(op));
-            }
+            else 
+                stack.push(Integer.parseInt(s));
         }
-        int sum = 0;
-        // use this for loop to iterate the stack
-        for (int score: stack) 
-            sum += score;
-        return sum;
+        int ans = 0;
+        while (!stack.isEmpty()) 
+            ans += stack.pop();
+        return ans;
     }
 }
