94ab2bbfe3081f5413cc46a0f6d3411a1dda90da
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-11 22:51:07 -0700

    Update distanceK.java

--- 863-AllNodesDistanceKinBinaryTree/distanceK.java
+++ 863-AllNodesDistanceKinBinaryTree/distanceK.java
@@ -14,45 +14,43 @@ After, we do a breadth first search to find all nodes a distance K from the targ
 */
 class Solution {
     public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        Map<TreeNode, List<TreeNode>> graph = new HashMap<>();
+        buildGraph(root, null, graph);
+        
         List<Integer> res = new ArrayList<>();
-        Map<TreeNode, List<TreeNode>> map = new HashMap<>();
-        buildMap(root, null, map);
-
-        Queue<TreeNode> q = new LinkedList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
         Set<TreeNode> visited = new HashSet<>();
-        q.add(target);
+        queue.offer(target);
         visited.add(target);
         
-        while (!q.isEmpty()) {
-            int levelSize = q.size();
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
             if (K == 0) {
-                for (int i = 0; i < levelSize; i++) 
-                    res.add(q.poll().val);
-                return res;
-            }
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode node = q.poll();
-                for (TreeNode next : map.get(node)) {   // get current node's parent and children
-                    if (!visited.contains(next)) {
-                        q.add(next);
-                        visited.add(next);
-                    }  
+                for (int i = 0; i < levelSize; i++)
+                    res.add(queue.poll().val);
+            } else {
+                for (int i = 0; i < levelSize; i++) {
+                    TreeNode curr = queue.poll();
+                    for (TreeNode nb : graph.get(curr))   // get current node's parent and children
+                        if (!visited.contains(nb)) {
+                            queue.offer(nb);
+                            visited.add(nb);
+                        }
                 }
             }
             K--;
         }
         return res;
     }
-
-    private void buildMap(TreeNode node, TreeNode parent, Map<TreeNode, List<TreeNode>> map) {
-        if (node == null) return;   // does not add leaf nodes' children
-        map.put(node, new ArrayList<TreeNode>());
-        if (parent != null) {    // does not map null with root node
-            map.get(node).add(parent); 
-            map.get(parent).add(node) ;
+    
+    private void buildGraph(TreeNode node, TreeNode parent, Map<TreeNode, List<TreeNode>> graph) {
+        if (node == null) return;           // does not map null with leaves
+        graph.put(node, new ArrayList<>());
+        if (parent != null) {               // does not map null with root node
+            graph.get(parent).add(node);
+            graph.get(node).add(parent);
         }
-        buildMap(node.left, node, map);
-        buildMap(node.right, node, map);
-        
-    } 
+        buildGraph(node.left, node, graph);
+        buildGraph(node.right, node, graph);
+    }
 }
