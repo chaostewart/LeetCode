e8057b72d5c29960e9456711c3c487c1225c15b0
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-14 12:59:11 -0700

    Update restoreIpAddresses.java

--- 093-RestoreIPAddresses/restoreIpAddresses.java
+++ 093-RestoreIPAddresses/restoreIpAddresses.java
@@ -13,17 +13,17 @@ class Solution {
 		    for(int b = 1 ; b < 4 ; ++b)
 	            for(int c = 1 ; c < 4 ; ++c)
 		            for(int d = 1 ; d < 4 ; ++d) {
-			            if(a + b + c + d == s.length()) {
-                            int n1 = Integer.parseInt(s.substring(0, a));
-                            int n2 = Integer.parseInt(s.substring(a, a + b));
-                            int n3 = Integer.parseInt(s.substring(a + b, a + b + c));
-                            int n4 = Integer.parseInt(s.substring(a + b + c));
-                            if (n1 <= 255 && n2 <= 255 && n3 <= 255 && n4 <= 255) {   // make sure n is in [0, 255]
-                                StringBuilder ip = new StringBuilder();
-                                ip.append(n1).append('.').append(n2).append('.').append(n3).append('.').append(n4);
-                                if (ip.length() == s.length() + 3)   // make sure didn't lose zero digits 
-                                    res.add(ip.toString());
-                            }
+						if (a + b + c + d == s.length()) {
+							int n1 = Integer.parseInt(s.substring(0, a));
+							int n2 = Integer.parseInt(s.substring(a, a + b));
+							int n3 = Integer.parseInt(s.substring(a + b, a + b + c));
+							int n4 = Integer.parseInt(s.substring(a + b + c));
+							if (n1 <= 255 && n2 <= 255 && n3 <= 255 && n4 <= 255) {   // make sure n is in [0, 255]
+								StringBuilder ip = new StringBuilder();
+								ip.append(n1).append('.').append(n2).append('.').append(n3).append('.').append(n4);
+								if (ip.length() == s.length() + 3)   // make sure didn't lose zero digits 
+									res.add(ip.toString());
+							}
                         }
 		            }
 		return res;
