4b2df380549e172ae9645562dbc4fbcd28bd4550
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-14 16:17:17 -0700

    Update getPermutation.java

--- 060-PermutationSequence/getPermutation.java
+++ 060-PermutationSequence/getPermutation.java
@@ -1,20 +1,21 @@
 // Time: O(n), space: O(n)
 class Solution {
-    public String getPermutation(int n, int k) {
-        StringBuilder sb = new StringBuilder();
-        ArrayList<Integer> num = new ArrayList<>();
-         
+    public String getPermutation(int n, int k) {       
+        ArrayList<Integer> nums = new ArrayList<>();       
         int fact = 1;
         for (int i = 1; i <= n; i++) {
             fact *= i;
-            num.add(i);
+            nums.add(i);
         }
+        // nums = {1,2,3,...,n},fact = n!
+        
         // https://leetcode.com/problems/permutation-sequence/discuss/22507/%22Explain-like-I'm-five%22-Java-Solution-in-O(n)
-        for (int i = 0, l = k - 1; i < n; i++) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0, j = k - 1; i < n; i++) {
             fact /= (n - i);
-            int index = (l / fact);
-            sb.append(num.remove(index));
-            l -= index * fact;
+            int index = j / fact;
+            sb.append(nums.remove(index));
+            j -= index * fact;
         }
         return sb.toString();
     }
