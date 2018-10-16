0a0b200dcbadc5146625fb56ef78f65264cac433
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-16 16:30:15 -0700

    Update nextPermutation.java

--- 031-NextPermutation/nextPermutation.java
+++ 031-NextPermutation/nextPermutation.java
@@ -4,7 +4,7 @@
 2. Exchange this number with the least number that's greater than this number.
 3. Reverse sort the numbers after the exchanged number.
 */
-
+// e.g. 2, 3, 6, 5, 4, 1
 class Solution {
     public void nextPermutation(int[] nums) {
         int i = nums.length - 2;
@@ -14,10 +14,12 @@ class Solution {
         if (i >= 0) {
             int j = nums.length - 1;
             while (j > i && nums[j] <= nums[i]) 
-                j--;    
+                j--;  
+            // i points to 3, j points to 4 --> 2, 4, 6, 5, 3, 1
             swap(nums, i, j);
         }
         // else (i == -1), no next permutation is possible
+        // --> 2, 4, 1, 3, 5, 6
         reverse(nums, i + 1);
     }
 
