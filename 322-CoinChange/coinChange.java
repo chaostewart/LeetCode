9312afaa7f46f67a481b6462890f0c4529cea414
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-17 14:10:48 -0700

    Update coinChange.java

--- 322-CoinChange/coinChange.java
+++ 322-CoinChange/coinChange.java
@@ -7,14 +7,13 @@ class Solution {
         int sum = 0;
         while(++sum <= amount) {
             int min = -1;    // default value is no solution to this 'sum' amount of money
-            for(int coin : coins) {
+            for(int coin : coins)
                 // value of sum is larger than coin value && previously value of (sum - coin) had a solution
                 if (sum >= coin && dp[sum - coin] != -1) {   
                     int temp = dp[sum-coin] + 1;
                     min = min < 0 ? temp : (temp < min ? temp : min);
                     // temp is either the first solution or a better solution
                 }
-            }
             dp[sum] = min;    // found the best solution for 'sum' money
         }
         return dp[amount];
