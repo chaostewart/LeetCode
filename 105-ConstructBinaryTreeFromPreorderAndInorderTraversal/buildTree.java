ff8222c3d2d9b9967729c6620bfcb6d263457c40
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-18 16:16:34 -0700

    Update buildTree.java

--- 105-ConstructBinaryTreeFromPreorderAndInorderTraversal/buildTree.java
+++ 105-ConstructBinaryTreeFromPreorderAndInorderTraversal/buildTree.java
@@ -7,6 +7,30 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+// newer version: preEnd is not necessary in the helpe function
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        Map<Integer, Integer> inMap = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++)
+            inMap.put(inorder[i], i);
+        return build(preorder, 0, inorder, 0, inorder.length - 1, inMap);
+    }
+    
+    private TreeNode build(int[] preorder, int preStart, int[] inorder, int inStart, int inEnd, Map<Integer, Integer> inMap) {
+        if (preStart > preorder.length - 1 || inStart > inEnd) 
+            return null;
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int rootIn = inMap.get(root.val);      // find the index of root in inorder array
+        int leftNum = rootIn - inStart;        // leftNum is the number of nodes in the left subtree of root
+        // build(preorder, root of left subtree, inorder, leftmost node in tree, rightmost node in left subtree, map)
+        root.left = build(preorder, preStart + 1, inorder, inStart, rootIn - 1, inMap);
+        // build(preorder, root of right subtree, inorder, leftmost node in right subtree, rightmost node in tree, map)
+        root.right = build(preorder, preStart + 1 + leftNum, inorder, rootIn + 1, inEnd, inMap);
+        return root;
+    }
+}
+
+
 class Solution {
     /*
     Node at preorder[0] is the root node.
