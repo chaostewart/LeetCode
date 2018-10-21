39072ee3613dcd12b1405bd78dbfc39c892a18c7
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-21 15:26:34 -0700

    Update validTree.java

--- 261-GraphValidTree/validTree.java
+++ 261-GraphValidTree/validTree.java
@@ -1,20 +1,25 @@
+// this problem is the same as find the number of disconnected continents
 class Solution {
     public boolean validTree(int n, int[][] edges) {
-        if (edges.length != n - 1) return false;  // tree of n nodes has n - 1 edges
-        int[] nums = new int[n];
-        Arrays.fill(nums, -1);
+        if (edges.length != n - 1) return false;        // tree of n nodes has n - 1 edges
+        int[] roots = new int[n];   
+        for (int i = 0; i < n; i++)
+            roots[i] = i;
         
-        for (int i = 0; i < edges.length; i++) {
-            int setA = find(nums, edges[i][0]);   
-            int setB = find(nums, edges[i][1]);
-            if (setA == setB) return false;   // there's a cycle
-            nums[setA] = setB;  // union two sets
+        for (int[] e : edges) {
+            int set0 = findRoots(roots, e[0]);
+            int set1 = findRoots(roots, e[1]);
+            if (set0 == set1) return false;             // there's a cycle
+            roots[set0] = roots[set1];                  // union two sets
         }
         return true;
     }
     
-    private int find(int[] nums, int i) {
-        if (nums[i] == -1) return i;
-        return find(nums, nums[i]);
+    private int findRoots(int[] roots, int i) {
+        while (roots[i] != i) {
+            roots[i] = roots[roots[i]];                 // path compression
+            i = roots[i];
+        }
+        return i;
     }
 }
