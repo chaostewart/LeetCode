2445fd9533166acb6760137a9189f8eb19c691c7
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-21 19:59:40 -0700

    Update reorganizeString.java

--- 767-ReorganizeString/reorganizeString.java
+++ 767-ReorganizeString/reorganizeString.java
@@ -1,3 +1,35 @@
+// a much faster solution when using a constant size of int array
+class Solution {
+    public String reorganizeString(String S) {
+        int[] freqs = new int[26];    
+        for(char c: S.toCharArray()) {       // count the frequency of each char
+            freqs[c-'a'] += 100;             // add 100 to freq for each apprearance
+            if (freqs[c-'a']/100 > (S.length() + 1) / 2)
+                return "";
+        }
+        
+        for(int i = 0; i < freqs.length; i++) 
+            freqs[i] += i;                   // add index to each value, to recover the index, simply do value % 100
+        Arrays.sort(freqs);
+        
+        char[] chars = new char[S.length()]; 
+        
+        int index = 1;
+        for(int freq: freqs){
+            int realFreq = freq/100;
+            char c = (char)('a' + (freq % 100));
+            
+            for(int i = 0; i < realFreq; i++) {
+                if(index >= S.length()) index = 0;         // this condition will be true for only once!
+                chars[index] = c;
+                index += 2;                 // fill the array with current char at every other index
+            }
+        }
+        return String.valueOf(chars);
+    }
+}
+
+
 class Solution {
     public String reorganizeString(String S) {
         // Create map of each char to its count
