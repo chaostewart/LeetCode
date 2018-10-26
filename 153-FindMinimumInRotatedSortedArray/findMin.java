22c2574cfc3b9597e16cf08828b88d242ae00bf3
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-26 12:45:12 -0700

    Update findMin.java

--- 153-FindMinimumInRotatedSortedArray/findMin.java
+++ 153-FindMinimumInRotatedSortedArray/findMin.java
@@ -1,36 +1,37 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while(left < right) {
+            if(nums[left] < nums[right]) 
+                return nums[left];
+            int mid = (left + right)/2;
+            if(nums[mid] > nums[right])
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        return nums[left];
+    }
+}
+
 // Time: O(logN), Space: O(1)
 class Solution {
     public int findMin(int[] nums) {
-        if (nums.length == 1)
-            return nums[0];
-
-        // initializing left and right pointers.
         int left = 0, right = nums.length - 1;
-
-        // if the last element is greater than the first element then there is no rotation.
-        if (nums[right] > nums[0]) 
-            return nums[0];   
-
-        // Binary search way
-        while (left <= right) {
-          // Find the mid element
-            int mid = left + (right - left) / 2;
-
-          // if the mid element is greater than its next element then mid+1 element is the smallest
-            if (nums[mid] > nums[mid + 1])
-                return nums[mid + 1];  
-
-          // if the mid element is lesser than its previous element then mid element is the smallest
-            if (nums[mid - 1] > nums[mid])
+        while (left < right) {
+            int mid = left + (right - left)/2;
+            if (nums[mid + 1] < nums[mid])
+                return nums[mid + 1];
+            if (mid - 1 >=0 && nums[mid - 1] > nums[mid])
                 return nums[mid];
-
-          // ans is in the right half
-            if (nums[mid] > nums[0])
+            if (nums[mid] > nums[right])
                 left = mid + 1;
-            else
-            // ans is in the left half
-                right = mid - 1;    
+            else if (nums[mid] < nums[right])
+                right = mid - 1;
         }
-        return -1;
+        return nums[left];
     }
 }
+
+
+
