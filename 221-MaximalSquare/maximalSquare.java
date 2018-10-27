fa1eafe191024302dac3f17a87285eb47bc26eca
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-27 15:37:09 -0700

    Update maximalSquare.java

--- 221-MaximalSquare/maximalSquare.java
+++ 221-MaximalSquare/maximalSquare.java
@@ -1,5 +1,25 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
+        if (matrix.length == 0) return 0;
+        int m = matrix.length, n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        int maxLen = 0;
+        for (int i = 0; i < m; i++)
+            for (int j = 0; j < n; j++)
+                if (matrix[i][j] == '1') {
+                    int left = i-1 >= 0 ? dp[i-1][j] : 0;
+                    int up = j-1 >= 0 ? dp[i][j-1] : 0;
+                    int upleft = i-1 >= 0 && j-1 >= 0 ? dp[i-1][j-1] : 0;
+                    dp[i][j] = 1 + Math.min(upleft, Math.min(up, left));
+                    maxLen = Math.max(maxLen, dp[i][j]);
+                }           
+        return maxLen * maxLen;
+    }
+}
+
+
+class Solution {
+    public int maximalSquare(char[][] matrix) {
         if(matrix.length == 0) return 0;
         int rows = matrix.length, cols = matrix[0].length;
         // dp[r][c] stores the length of a square of which its bottom right cell is at (r, c)
