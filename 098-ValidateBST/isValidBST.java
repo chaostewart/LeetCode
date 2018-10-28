4581b1b88a7a442992d142a54e0f2659d4a0d7d4
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-28 16:05:41 -0700

    Update isValidBST.java

--- 098-ValidateBST/isValidBST.java
+++ 098-ValidateBST/isValidBST.java
@@ -7,6 +7,26 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        Integer prev = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (prev != null && root.val <= prev)
+                return false;
+            prev = root.val;
+            root = root.right;           
+        }
+         return true;
+    }
+}
+
 class Solution {
     public boolean isValidBST(TreeNode root) {
         // Using Stack to do an inorder traversal, note all nodes are touched from the smallest value to the largest
