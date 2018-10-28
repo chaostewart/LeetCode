3e68fcfe203581806cc31a3b410de58e5c692f59
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-27 17:32:43 -0700

    Add files via upload

---
+++ 162-FindPeakElement/findPeakElement.java
@@ -0,0 +1,18 @@
+/*
+Each local maximum is one valid peak.
+Find a local maximum with binary search.
+Time: O(logn)
+*/
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int lo = 0, hi = nums.length - 1;      
+        while(lo < hi) {
+            int mid = lo + (hi - lo)/2;
+            if (nums[mid] < nums[mid + 1])
+                lo = mid + 1;
+            else
+                hi = mid;
+        }
+        return lo;
+    }
+}
