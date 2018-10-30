5137c8ef4caa865cc5d9e324407c5be63b974947
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-10-30 12:35:55 -0700

    Update isValid.java

--- 020-ValidParentheses/isValid.java
+++ 020-ValidParentheses/isValid.java
@@ -9,9 +9,9 @@ class Solution {
             else if (i == '[')
                 stack.push(']');
             // check empty method is named empty() in original class but is named isEmpty() of Collection interface
-            else if (stack.empty() || i != stack.pop()) // cases where there are more closing brackets OR brackets don't match
+            else if (stack.isEmpty() || i != stack.pop()) // cases where there are more closing brackets OR brackets don't match
                 return false;
         }
-        return stack.empty();  // true == all brackets match || false == there are more opening brackets
+        return stack.isEmpty();  // true == all brackets match || false == there are more opening brackets
     }
 }
