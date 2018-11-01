db69a330e51ec3febd0dc72d8ff690cb1c52b1d3
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-11-01 13:09:33 -0700

    Update findCheapestPrice.java

--- 787-CheapestFlightsWithinKStops/findCheapestPrice.java
+++ 787-CheapestFlightsWithinKStops/findCheapestPrice.java
@@ -5,14 +5,14 @@ class Solution {
         Arrays.fill(prices, Integer.MAX_VALUE);
         prices[src] = 0;
         //run it k+1 iterations.
-        for (int i = 0; i <= K; i++) {
-            int[] temp = Arrays.copyOf(prices, n);    // make a copy here due to num of stops restriction
+        while(K-- >= 0) {
+            int[] nextRound = Arrays.copyOf(prices, n);    // make a copy here due to num of stops restriction
             for (int[] flight : flights) {
-                int curr = flight[0], next = flight[1], price = flight[2];
-                if (prices[curr] == Integer.MAX_VALUE) continue;
-                temp[next] = Math.min(temp[next], prices[curr] + price);
+                int from = flight[0], to = flight[1], price = flight[2];
+                if (prices[from] != Integer.MAX_VALUE)
+                    nextRound[to] = Math.min(nextRound[to], prices[from] + price);
             }
-            prices = temp;
+            prices = nextRound;
         }
         return prices[dst] == Integer.MAX_VALUE ? -1 : prices[dst];
     }
