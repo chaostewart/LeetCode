29b860e13985890614281ad6bdb260a49cc8512c
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-11-08 15:16:31 -0800

    Update coinChange.java

--- 322-CoinChange/coinChange.java
+++ 322-CoinChange/coinChange.java
@@ -1,14 +1,25 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];     // dp[amount] stores the final answer
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                // value of amount is larger than coin value && previously value of (i - coin) had a solution
+                if (c <= i && dp[i-coin] != Integer.MAX_VALUE)
+                    dp[i] = Math.min(dp[i-coin] + 1, dp[i]);
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+        /*
         if (amount < 1) 
             return 0;
-        int[] dp = new int[amount + 1];   // dp[amount] stores the final answer
-        
+        int[] dp = new int[amount + 1];      
         int sum = 0;
         while(++sum <= amount) {
             int min = -1;    // default value is no solution to this 'sum' amount of money
             for(int coin : coins)
-                // value of sum is larger than coin value && previously value of (sum - coin) had a solution
+                
                 if (sum >= coin && dp[sum - coin] != -1) {   
                     int temp = dp[sum-coin] + 1;
                     min = min < 0 ? temp : (temp < min ? temp : min);
@@ -17,5 +28,6 @@ class Solution {
             dp[sum] = min;    // found the best solution for 'sum' money
         }
         return dp[amount];
+        */
     }
 }
