0792a19e94ae00b964dfe1bf2197e51065430566
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-11-12 13:31:25 -0800

    Update maxSubArray.java

--- 053-MaximumSubarray/maxSubArray.java
+++ 053-MaximumSubarray/maxSubArray.java
@@ -6,7 +6,8 @@ class Solution {
         
         for(int i = 1; i < nums.length; i++){
             // if previous subarray has sum larger than 0, then attach the current to previous
-            dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+            //dp[i] = nums[i] + (dp[i - 1] > 0 ? dp[i - 1] : 0);
+            dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
             res = Math.max(res, dp[i]);
         }
         
