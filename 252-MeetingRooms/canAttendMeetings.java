8cf8728fc8ffd8683987388cd9a6ebf15ae37e2a
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-11-13 13:08:50 -0800

    Update canAttendMeetings.java

--- 252-MeetingRooms/canAttendMeetings.java
+++ 252-MeetingRooms/canAttendMeetings.java
@@ -9,7 +9,7 @@
  */
 class Solution {
     public boolean canAttendMeetings(Interval[] intervals) {
-        if (intervals == null || intervals.length == 0) return true;
+        //if (intervals == null || intervals.length == 0) return true;
         Arrays.sort(intervals, (a, b) -> a.start - b.start);
         for (int i = 1; i < intervals.length; i++)
             if (intervals[i].start < intervals[i - 1].end)
