582ad1220cfea37ce0761a52e5670b9d5e244be7
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-11-20 15:34:38 -0800

    Update reverse.java

--- 007-ReverseInteger/reverse.java
+++ 007-ReverseInteger/reverse.java
@@ -2,8 +2,7 @@ class Solution {
     public int reverse(int x) {
         int result = 0;
         while(x != 0) {
-            int remainder = x % 10;
-            int temp = result * 10 + remainder;
+            int temp = result * 10 + x % 10;
             if(temp / 10 != result) return 0;
             result = temp;
             x /= 10;
