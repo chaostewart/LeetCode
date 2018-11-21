d5274552103a9b3d5274870e38b5d6550f76ea2d
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-11-20 22:49:29 -0800

    Update validTree.java

--- 261-GraphValidTree/validTree.java
+++ 261-GraphValidTree/validTree.java
@@ -1,5 +1,23 @@
 // this problem is the same as find the number of disconnected continents
 class Solution {
+    // another way of dealing with disconnected components
+    public boolean validTree(int n, int[][] edges) {
+        int[] roots = new int[n];
+        int count = n;
+        for (int i = 0; i < n; i++) 
+            roots[i] = i;
+        for (int[] e: edges) {
+            int root0 = findRoots(roots, e[0]);
+            int root1 = findRoots(roots, e[1]);
+            if (root0 ==root1)
+                return false;
+            roots[root0] = root1;
+            count--;
+            
+        }
+        return count == 1;
+    }
+    
     public boolean validTree(int n, int[][] edges) {
         if (edges.length != n - 1) return false;        // tree of n nodes has n - 1 edges
         int[] roots = new int[n];   
