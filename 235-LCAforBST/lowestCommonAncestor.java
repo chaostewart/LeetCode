c182a14efc559af75e3d384fe9a5859191793715
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2018-11-25 18:57:02 -0800

    Update lowestCommonAncestor.java

--- 235-LCAforBST/lowestCommonAncestor.java
+++ 235-LCAforBST/lowestCommonAncestor.java
@@ -8,6 +8,13 @@
  * }
  */
 class Solution {
+    // recursive solution
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if ((p.val - root.val)*(q.val - root.val) > 0) 
+            return lowestCommonAncestor(p.val < root.val ? root.left : root.right, p, q);       
+        return root;
+    }
+    
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         // this is not just a binary tree...a BST has its left child smaller than it, and right child larger than it
         // here comes a genius's code...OMG..
