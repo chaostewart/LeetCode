f6ac25052f043de08c5d0479cc101d432bba4461
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2019-01-01 22:38:34 -0800

    Update isBalanced.java

--- 110-BalancedBinaryTree/isBalanced.java
+++ 110-BalancedBinaryTree/isBalanced.java
@@ -7,6 +7,23 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        int[] res = new int[1];
+        getDepth(root, res);
+        return res[0] <= 1;
+    }
+    
+    private int getDepth(TreeNode root, int[] res) {
+        if (root == null) return 0;
+        int left = getDepth(root.left, res);
+        int right = getDepth(root.right, res);
+        res[0] = Math.max(res[0], Math.abs(left - right));
+        return 1 + Math.max(left, right);
+    }
+}
+
 class Solution {
     public boolean isBalanced(TreeNode root) {
         if (root == null) return true;
