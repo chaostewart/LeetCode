a741d292a1b06a5fda6950f6be38db060d367788
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2019-02-02 23:03:03 -0800

    Add files via upload

---
+++ 109-ConvertSortedListToBST/sortedListToBST.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+ // Time complexity: O(nlogn)
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head == null) return null;
+        return toBST(head, null);
+    }
+    
+    private TreeNode toBST(ListNode head, ListNode tail) {
+        if(head == tail) return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast != tail && fast.next != tail) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        TreeNode root = new TreeNode(slow.val);
+        root.left = toBST(head, slow);
+        root.right = toBST(slow.next, tail);
+        return root;
+    }
+}
