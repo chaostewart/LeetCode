857c39bc850810426beaa043ffbaecd280a14555
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2019-02-04 18:17:34 -0800

    Update distanceK.java

--- 863-AllNodesDistanceKinBinaryTree/distanceK.java
+++ 863-AllNodesDistanceKinBinaryTree/distanceK.java
@@ -8,6 +8,58 @@
  * }
  */
 /*
+Instead of building a whole undirected graph, only store reversed edges in map
+this speeds up from 16% to 65%
+*/
+class Solution {
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int K) {
+        Map<TreeNode, TreeNode> map = new HashMap<>();
+        buildBackEdges(root, null, map);
+        List<Integer> ans = new ArrayList<>();
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(target);
+        Set<TreeNode> visited = new HashSet<>();
+        visited.add(target);
+        int count = 0;
+        while (!queue.isEmpty()) {         
+            if (count == K) {
+                while(!queue.isEmpty())
+                    ans.add(queue.poll().val);
+                return ans;
+            }
+            
+            int size = queue.size();
+            while(size-->0) {
+                TreeNode curr = queue.poll();
+                if (curr.left != null && !visited.contains(curr.left)) {
+                    queue.add(curr.left);
+                    visited.add(curr.left);
+                }
+                if (curr.right != null && !visited.contains(curr.right)) {
+                    queue.add(curr.right);
+                    visited.add(curr.right);
+                }
+                if (map.get(curr) != null && !visited.contains(map.get(curr))) {
+                    queue.add(map.get(curr));
+                    visited.add(map.get(curr));
+                }               
+            }
+            count++;                     
+        }
+        return ans;
+    }
+    
+    private void buildBackEdges(TreeNode node, TreeNode parent, Map<TreeNode, TreeNode> map) {
+        if (node == null) return;
+        if (parent != null)
+            map.put(node, parent);
+        buildBackEdges(node.left, node, map);
+        buildBackEdges(node.right, node, map);
+    }
+}
+
+/*
 We first do a depth first search where we map every node with its parent and children 
 -- build undirected map
 After, we do a breadth first search to find all nodes a distance K from the target.
