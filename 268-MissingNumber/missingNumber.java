9646e9cfec873f4ca45a109eec1f3c4875b607a5
Chao (Chelsea) Li <chelseachaoli@gmail.com>
2019-02-06 16:25:45 -0800

    Update missingNumber.java

--- 268-MissingNumber/missingNumber.java
+++ 268-MissingNumber/missingNumber.java
@@ -1,3 +1,15 @@
+// similiar to 136. Single Number problem
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans ^= i;
+            ans ^= nums[i];
+        }
+        return ans^n;
+    }
+}
 /*
 since the n numbers are from [0, n], we can just add all the (n+1) numbers from [0, n] together 
 and minus the sum of the n numbers in array.
